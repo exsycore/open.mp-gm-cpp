6cc0fa1946d91aa7fbac2cfaec6e2402f21b65be
EasyCore <131441651+exsycore@users.noreply.github.com>
2025-05-15 13:25:26 +0700

    Second commit

--- build/lib/sdk/lib/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/omp-nx-cpp/lib/sdk/lib/glm
-# Build directory: D:/omp-nx-cpp/build/lib/sdk/lib/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
